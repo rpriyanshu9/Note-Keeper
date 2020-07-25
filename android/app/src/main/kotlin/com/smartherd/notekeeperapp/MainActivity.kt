2ff82f1a96e2bbfeddb7ebccdd324f75dfe19b0c
Priyanshu Raj <rpriyanshu9@gmail.com>
2020-07-25 10:28:35 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/smartherd/notekeeperapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.smartherd.notekeeperapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/database_helper_dart.dart
@@ -0,0 +1,111 @@
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:notekeeperapp/models/note.dart';
+
+class DatabaseHelper {
+
+  static DatabaseHelper _databaseHelper;    // Singleton DatabaseHelper
+  static Database _database;                // Singleton Database
+  // Singleton makes sure that only one instance of this class is ever created in the program
+
+  String noteTable = 'note_table';
+  String colId = 'id';
+  String colTitle = 'title';
+  String colDescription = 'description';
+  String colPriority = 'priority';
+  String colDate = 'date';
+
+  DatabaseHelper._createInstance(); // Named constructor to create instance of DatabaseHelper
+
+  factory DatabaseHelper() {  // On using factory keyword before the constructor, it will allow to return some value
+
+    if (_databaseHelper == null) {
+      _databaseHelper = DatabaseHelper._createInstance(); // This is executed only once, singleton object
+    }
+    return _databaseHelper;
+  }
+
+  Future<Database> get database async {
+
+    if (_database == null) { // since the _database object is singleton, we need to have a null check
+      _database = await initializeDatabase();
+    }
+    return _database;
+  }
+
+  Future<Database> initializeDatabase() async {
+    // Get the directory path for both Android and iOS to store database.
+    Directory directory = await getApplicationDocumentsDirectory(); // Part of path provider package
+    String path = directory.path + 'notes.db';
+
+    // Open/create the database at a given path
+    var notesDatabase = await openDatabase(path, version: 1, onCreate: _createDb); // "version" ensures that onCreate can be called here.
+    // openDatabase is part of sqflite package.
+    return notesDatabase;
+    // after this we need to create a getter for the Database reference variable
+  }
+
+  void _createDb(Database db, int newVersion) async {  // Function to create the new database
+    // now we need some helper function to initialise our database.
+    // this is achieved by creating initializeDatabase()
+
+    await db.execute('CREATE TABLE $noteTable($colId INTEGER PRIMARY KEY AUTOINCREMENT, $colTitle TEXT, '
+        '$colDescription TEXT, $colPriority INTEGER, $colDate TEXT)'); // normal SQL statement
+  }
+
+  // Fetch Operation: Get all note objects from database
+  Future<List<Map<String, dynamic>>> getNoteMapList() async {
+    Database db = await this.database;
+
+//		var result = await db.rawQuery('SELECT * FROM $noteTable order by $colPriority ASC');
+    var result = await db.query(noteTable, orderBy: '$colPriority ASC');
+    return result;
+  }
+
+  // Insert Operation: Insert a Note object to database
+  Future<int> insertNote(Note note) async {
+    Database db = await this.database;
+    var result = await db.insert(noteTable, note.toMap());
+    return result;
+  }
+
+  // Update Operation: Update a Note object and save it to database
+  Future<int> updateNote(Note note) async {
+    var db = await this.database;
+    var result = await db.update(noteTable, note.toMap(), where: '$colId = ?', whereArgs: [note.id]);
+    return result;
+  }
+
+  // Delete Operation: Delete a Note object from database
+  Future<int> deleteNote(int id) async {
+    var db = await this.database;
+    int result = await db.rawDelete('DELETE FROM $noteTable WHERE $colId = $id');
+    return result;
+  }
+
+  // Get number of Note objects in database
+  Future<int> getCount() async {
+    Database db = await this.database;
+    List<Map<String, dynamic>> x = await db.rawQuery('SELECT COUNT (*) from $noteTable');
+    int result = Sqflite.firstIntValue(x);
+    return result;
+  }
+
+  // Get the 'Map List' [ List<Map> ] and convert it to 'Note List' [ List<Note> ]
+  Future<List<Note>> getNoteList() async {
+
+    var noteMapList = await getNoteMapList(); // Get 'Map List' from database
+    int count = noteMapList.length;         // Count the number of map entries in db table
+
+    List<Note> noteList = List<Note>();
+    // For loop to create a 'Note List' from a 'Map List'
+    for (int i = 0; i < count; i++) {
+      noteList.add(Note.fromMapObject(noteMapList[i]));
+    }
+
+    return noteList;
+  }
+
+}
